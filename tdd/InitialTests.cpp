8d91e1af02bbf6580ea13094ce309772e1f5c938
MiddleRaster <MiddleRaster@users.noreply.github.com>
2026-06-17 10:48:45 -0400

    added ODR violation report for UDTs

--- tdd/InitialTests.cpp
+++ tdd/InitialTests.cpp
@@ -316,7 +316,66 @@ struct OdrViolationReporter
                 }
             }
         }
-        return 1;
+        return violationCount;
+    }
+    static int ReportFunctionOdrViolations(const std::map<std::string, std::vector<OdrCop2::UdtInfo>>& map, auto&& out)
+    {
+        /*
+        ODR VIOLATION: S
+        [tu1.cpp]
+        struct S {
+        public:  int x;
+        };
+        [tu2.cpp]
+        struct S {
+        public:  long x;
+        };
+        */
+
+        int violationCount = 0;
+        for (auto& [name, items] : map)
+        {
+            if (items.size() < 2)
+                continue;
+
+            //if (true == skipAnonymous(items[0]))
+            //    continue;
+
+            if (std::all_of(items.begin() + 1, items.end(), [&](const auto& x) { return x == items[0]; }))
+                continue;
+
+            // find mismatch index
+            //int mismatch = -1;
+            //for (size_t m = 1; m < items.size(); ++m)
+            //{
+            //    if (-1 != (mismatch = getMismatchIndex(items[0], items[m])))
+            //        break;
+            //}
+
+            ++violationCount;
+            out << "ODR VIOLATION: " << name << '\n';
+
+            std::vector<bool> printed(items.size(), false);
+            for (size_t i=0; i<items.size(); ++i)
+            {
+                if (printed[i])
+                    continue;
+
+                out << "[" << items[i].TU << "]\n";
+                out        << items[i].fullyQualified << ";\n";
+                printed[i] = true;
+
+                for (size_t j=i+1; j<items.size(); ++j)
+                {
+                    if (!printed[j] && (items[i] == items[j]))
+                    {
+                        out << "[" << items[j].TU << "] - same as above\n";
+                        printed[j] = true;
+                    }
+                }
+            }
+        }
+        return violationCount;
     }
 };
 
@@ -388,6 +447,34 @@ Test ExploringOdrViolationReportingTests[] =
             int violations = OdrViolationReporter::ReportFunctionOdrViolations(maps.functionMap, oss);
             Assert::AreEqual(1, violations, "should have been one ODR violation");
             Assert::AreEqual("ODR VIOLATION: ?foo@@YAXXZ\n  [tu1.cpp]\n    inline void __cdecl foo()\n  [tu2.cpp]\n    void __cdecl foo()\n  [tu3.cpp] - same as above\n", oss.str());
+        }
+    },
+    {"Report UDT ODR violations", []
+        {
+            std::string code1 = "struct S { int  x; };";
+            std::string code2 = "struct S { long x; };";
+
+            OdrCop2::AllMaps maps;
+            Assert::AreEqual(true, clang::tooling::runToolOnCodeWithArgs(std::make_unique<OdrCop2::VisitorAction>(maps), code1, { "-x", "c++" }, "tu1.cpp"));
+            Assert::AreEqual(true, clang::tooling::runToolOnCodeWithArgs(std::make_unique<OdrCop2::VisitorAction>(maps), code2, { "-x", "c++" }, "tu2.cpp"));
+
+            Assert::AreEqual(1, maps.udtMap.size(), "should be only one UDT name");
+
+            const auto& vec = maps.udtMap.begin()->second;
+            Assert::AreNotEqual(vec[0].fullyQualified, vec[1].fullyQualified, "first and second UDT names are different");
+
+            std::ostringstream oss;
+            int violations = OdrViolationReporter::ReportFunctionOdrViolations(maps.udtMap, oss);
+            Assert::AreEqual(1, violations, "should have been one ODR violation");
+            Assert::AreEqual("ODR VIOLATION: S\n"
+                             "[tu1.cpp]\n"
+                             "struct S {\n"
+                             "public:    int x;\n"
+                             "};\n"
+                             "[tu2.cpp]\n"
+                             "struct S {\n"
+                             "public:    long x;\n"
+                             "};\n", oss.str(), "should have been one ODR violation");
         }
     },
 };
